5ab621fda05ab8377b5b55a94799732520311f3b
kiya <kiya@gmail.com>
2022-11-11 16:46:31 +0900

    05_bs 더하기 연산 필요 없음! 삭제

--- gittest/src/com/git/run/Main.java
+++ gittest/src/com/git/run/Main.java
@@ -5,9 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Git!!");
-		int a=10;
-		int b=20;
-		System.out.println(a+b);
+
 	}
 
 }
