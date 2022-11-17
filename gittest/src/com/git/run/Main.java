6a2c0fcb64e8a3c7df83a595e3530939e0e5c957
kiya <kiya@gmail.com>
2022-11-17 09:50:08 +0900

    bs main 12번째수정

--- gittest/src/com/git/run/Main.java
+++ gittest/src/com/git/run/Main.java
@@ -3,12 +3,13 @@ package com.git.run;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Hello Git!!");
 		
 		int a = 10;
 		String test = "유병승";
 		System.out.println(a+test);
+
+		System.out.println("안녕하세요~ 오늘은 git입니다.");
 
 	}
 
