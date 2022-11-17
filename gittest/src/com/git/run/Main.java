5be9f34702f50890404d42627f0eb504fcea9669
kiya <kiya@gmail.com>
2022-11-17 09:51:13 +0900

    12번째라인 변수추가 후 수정

--- gittest/src/com/git/run/Main.java
+++ gittest/src/com/git/run/Main.java
@@ -3,12 +3,14 @@ package com.git.run;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Hello Git!!");
 		
 		int a = 10;
 		String test = "유병승";
 		System.out.println(a+test);
+		
+		String title="내용";
+		System.out.println(title+"이건 어떻게 수정?");
 
 	}
 
